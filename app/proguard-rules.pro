fa688099042af05646e1e89d120997d19e4c6d4f
Can Yapan <canyapan@gmail.com>
2017-03-07 18:02:03 +0300

    replacing ACRA with Fabric Crashlytics activating minify finally, thanks to fabric which supports ProGuard.

--- app/proguard-rules.pro
+++ app/proguard-rules.pro
@@ -16,28 +16,13 @@
 #   public *;
 #}
 
-# keep this class so that logging will show 'ACRA' and not a obfuscated name like 'a'.
-# Note: if you are removing log messages elsewhere in this file then this isn't necessary
--keep class org.acra.ACRA { *; }
+-keepattributes *Annotation*
+-keepattributes SourceFile,LineNumberTable
 
-# keep this around for some enums that ACRA needs
--keep class org.acra.ReportingInteractionMode { *; }
+-keep public class * extends java.lang.Exception
 
--keepnames class org.acra.sender.HttpSender$** { *; }
+-keep class com.crashlytics.** { *; }
 
--keepnames enum org.acra.ReportField { *; }
-
-# keep this otherwise it is removed by ProGuard
--keep public class org.acra.ErrorReporter {
-    public void addCustomData(java.lang.String,java.lang.String);
-    public void putCustomData(java.lang.String,java.lang.String);
-    public void removeCustomData(java.lang.String);
-}
-
-# keep this otherwise it is removed by ProGuard
--keep public class org.acra.ErrorReporter {
-    public void handleSilentException(java.lang.Throwable);
-}
-
+-dontwarn com.crashlytics.**
 -dontwarn java.beans.**
 -dontwarn com.opencsv.**
--- app/src/main/java/com/canyapan/dietdiaryapp/Application.java
+++ app/src/main/java/com/canyapan/dietdiaryapp/Application.java
@@ -1,25 +1,9 @@
 package com.canyapan.dietdiaryapp;
 
+import com.crashlytics.android.Crashlytics;
+import io.fabric.sdk.android.Fabric;
 import net.danlew.android.joda.JodaTimeAndroid;
 
-import org.acra.ACRA;
-import org.acra.ReportingInteractionMode;
-import org.acra.annotation.ReportsCrashes;
-
-@ReportsCrashes(
-        formUri = BuildConfig.ACRA_URL,
-        reportType = org.acra.sender.HttpSender.Type.JSON,
-        httpMethod = org.acra.sender.HttpSender.Method.PUT,
-        formUriBasicAuthLogin = BuildConfig.ACRA_USERNAME,
-        formUriBasicAuthPassword = BuildConfig.ACRA_PASSWORD,
-        mode = ReportingInteractionMode.DIALOG,
-        resToastText = R.string.crash_toast_text, // optional, displayed as soon as the crash occurs, before collecting data which can take a few seconds
-        resDialogText = R.string.crash_dialog_text,
-        resDialogIcon = android.R.drawable.ic_dialog_info, //optional. default is a warning sign
-        resDialogTitle = R.string.crash_dialog_title, // optional. default is your application name
-        resDialogCommentPrompt = R.string.crash_dialog_comment_prompt, // optional. When defined, adds a user text field input with this text resource as a label
-        resDialogOkToast = R.string.crash_dialog_ok_toast // optional. displays a Toast message when the user accepts to send a report.
-)
 public class Application extends android.app.Application {
     public static final String APP_DIR = "DietDiaryApp";
 
@@ -29,8 +13,8 @@ public class Application extends android.app.Application {
 
         JodaTimeAndroid.init(this);
 
-        if (BuildConfig.ACRA_ACTIVE) {
-            ACRA.init(this);
+        if (BuildConfig.CRASHLYTICS_ENABLED) {
+            Fabric.with(this, new Crashlytics());
         }
     }
 }
